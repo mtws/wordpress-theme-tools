f5d390a5fc587421108858754e70dca26d43971e
Mats Tuovinen <mats.tuovinen@ilmedia.fi>
2016-01-27 13:32:44 +0200

    Add Ajax check

--- inc/disable-wp-admin-for-subscribers.php
+++ inc/disable-wp-admin-for-subscribers.php
@@ -4,9 +4,13 @@
  * WP admin for Subscribers.
  */
 function mtws_redirect_admin(){
-    if ( ! current_user_can( 'edit_posts' ) ){
-        wp_redirect( site_url() );
-        exit;       
+    // check if Ajax request
+    if( empty($_SERVER['HTTP_X_REQUESTED_WITH']) || strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) !== 'xmlhttprequest' ) {
+        // proceed if was not Ajax request
+        if ( ! current_user_can( 'edit_posts' ) ){
+            wp_redirect( site_url() );
+            exit;       
+        }
     }
 }
 add_action( 'admin_init', 'mtws_redirect_admin' );
