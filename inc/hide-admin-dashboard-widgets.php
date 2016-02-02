0267be96173c79012cfc67a7aa5b3afe14c54ca5
Mats Tuovinen <mats.tuovinen@ilmedia.fi>
2016-02-02 16:27:22 +0200

    Add more examples

--- inc/hide-admin-dashboard-widgets.php
+++ inc/hide-admin-dashboard-widgets.php
@@ -16,6 +16,7 @@
 //$wp_meta_boxes['dashboard']['side']['core']['dashboard_secondary']
 
 function mtws_remove_dashboard_widgets() {
+    //remove_meta_box( 'dashboard_activity', 'dashboard', 'normal' );
     remove_meta_box( 'dashboard_quick_press', 'dashboard', 'side' );
     remove_meta_box( 'dashboard_primary', 'dashboard', 'side' );
     remove_meta_box( 'dashboard_secondary', 'dashboard', 'side' );
@@ -28,3 +29,6 @@ if (!current_user_can( 'manage_options' )) {
     // Hide widgets for all other users than admins
     add_action('wp_dashboard_setup', 'mtws_remove_dashboard_widgets' );
 }
+
+// More info and a guide on how to create your own dashboard widgets:
+// https://codex.wordpress.org/Dashboard_Widgets_API
